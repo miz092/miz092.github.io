3ab0fcc22d9e7b2418d9979af1f0d4d206f4c04f
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-26 16:37:50 +0100

    init app.jsx and the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/homepage/HomePage.jsx";
+import Experience from "./pages/experience/Experience.jsx";
+import Navbar from "./pagecomponents/navbar/Navbar.jsx";
+import Footer from "./pagecomponents/footer/Footer.jsx";
+import ProjectDisplay from "./pages/projectdisplay/ProjectDisplay.jsx";
+import Projects from "./pages/projects/Projects.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
