ba24ce0a34ade66f7ef28139f55179f1746a341c
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-26 19:36:54 +0100

    implement verticaltimelineelement but needs to be finished

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import "./Experience.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { color } from "@mui/system";
+import { MdSchool } from "react-icons/md";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="portfolio__experience">
+      <VerticalTimeline lineColor="#006d77">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2012"
+          iconStyle={{ background: "#006d77", color: "#fff" }}
+          icon={<MdSchool></MdSchool>}
+        >
+          <h3 className="vertical-timeline-element--title">My highschool</h3>
+          <p>High school diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2012"
+          iconStyle={{ background: "#006d77", color: "#fff" }}
+          icon={<MdSchool></MdSchool>}
+        >
+          <h3 className="vertical-timeline-element--title">My highschool</h3>
+          <p>High school diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2012"
+          iconStyle={{ background: "#006d77", color: "#fff" }}
+          icon={<MdSchool></MdSchool>}
+        >
+          <h3 className="vertical-timeline-element--title">My highschool</h3>
+          <p>High school diploma</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
