e9f2057b9448555f606cfb7decc35185c166399b
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-26 17:38:57 +0100

    implement basic navbar styling and responsiveness

--- src/pagecomponents/navbar/Navbar.jsx
+++ src/pagecomponents/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { ReorderTwoTone } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import "./Navbar.css";
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div className="portfolio__navbar">
-      <div>
-        <button className="portfolio__navbar_toggleButton"></button>
+    <div className="portfolio__navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="portfolio__navbar_toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderTwoTone />
+        </button>
       </div>
       <div className="portfolio__navbar_links">
         <Link to={"/"}>Home</Link>
