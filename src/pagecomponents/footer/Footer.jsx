6174aa1ec00c520a861661a2842be96d9de78796
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-26 17:57:13 +0100

    implement the footer with social media icons and basic design

--- src/pagecomponents/footer/Footer.jsx
+++ src/pagecomponents/footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import "./footer.css";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="portfolio__footer">
+      <div className="portfolio__footer_socialMedia">
+        <BsGithub />
+        <BsInstagram />
+        <BsLinkedin />
+        <MdEmail />
+      </div>
+      <p>&copy; 2023 Mi Zo</p>
+    </div>
+  );
 }
 
 export default Footer;
