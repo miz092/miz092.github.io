f8aa8cc57621a715f2924ad08e418ad2230b69ea
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-28 11:21:09 +0100

    Updates

--- src/pagecomponents/styledbutton/StyledButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./StyledButton.css";
-function StyledButton({ text }) {
-  return <button className="bn-32 bn32">{text}</button>;
-}
-
-export default StyledButton;
