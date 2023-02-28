242ef4e1cfc995f5a39c2835b3cfb498441f9fc2
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-28 10:29:01 +0100

    fix main.jsx base path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< Updated upstream
-})
-=======
   base: "/myportfolio/",
 });
->>>>>>> Stashed changes
