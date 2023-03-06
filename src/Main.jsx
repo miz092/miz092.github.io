2b17fb1a40a70e2b8b7224d949a0dd6fea3309b0
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-06 13:20:59 +0100

    revert last change

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,10 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const App = require("./App");
-require("./index.css");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
