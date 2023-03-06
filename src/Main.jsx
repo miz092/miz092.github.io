fa3880f0fb15093ab7969b0d6aea329e4d42d131
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-06 13:13:15 +0100

    change main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+const React = require("react");
+const ReactDOM = require("react-dom");
+const App = require("./App");
+require("./index.css");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
