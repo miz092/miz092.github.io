1c8c4c6bcee4a1d5a2f21a5257957aa343a2869c
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-07 17:58:14 +0100

    add css changes for smooth responsiveness

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -6,26 +6,29 @@ import spaceVideo from "../../assets/space-7983.mp4";
 
 function Projects() {
   return (
-    <div className="portfolio__projects">
-      <h2> My personal projects</h2>
-      <div className="portfolio__projects_projectList">
+    <>
+      {" "}
+      <div className="portfolio__projects">
         <video id="bg-video1" autoPlay playsInline loop muted>
           <source src={spaceVideo} type="video/mp4" />
         </video>
-        {ProjectList.map((project, index) => {
-          return (
-            <div>
-              {" "}
-              <ProjectPreview
-                id={index}
-                name={project.name}
-                image={project.image}
-              />
-            </div>
-          );
-        })}
+        <h2> My personal projects</h2>
+        <div className="portfolio__projects_projectList">
+          {ProjectList.map((project, index) => {
+            return (
+              <div>
+                {" "}
+                <ProjectPreview
+                  id={index}
+                  name={project.name}
+                  image={project.image}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
