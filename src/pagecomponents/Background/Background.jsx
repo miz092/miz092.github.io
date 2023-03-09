5c2ff9e0c2cb65073049410b8457d1d96f5f731c
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-09 18:14:44 +0100

    fix background video twitching on video change

--- src/pagecomponents/Background/Background.jsx
+++ src/pagecomponents/Background/Background.jsx
@@ -6,18 +6,22 @@ import video2 from "../../assets/video-2.mp4";
 import video3 from "../../assets/video-3.mp4";
 import video4 from "../../assets/video-4.mp4";
 import video5 from "../../assets/video-5.mp4";
-
 import video6 from "../../assets/video-7.mp4";
 
 function Background() {
   const videos = [video1, video2, video3, video4, video5, video6];
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [showVideo, setShowVideo] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentVideoIndex(
-        (currentIndex) => (currentIndex + 1) % videos.length
-      );
+      setShowVideo(false);
+      setTimeout(() => {
+        setCurrentVideoIndex(
+          (currentIndex) => (currentIndex + 1) % videos.length
+        );
+        setShowVideo(true);
+      }, 1000); // wait 2 seconds before showing the new video
     }, 5000);
 
     return () => clearInterval(intervalId);
@@ -34,6 +38,8 @@ function Background() {
         loop
         muted
         src={currentVideoSrc}
+        className={showVideo ? "show" : "hide"} // add class based on visibility state
+        onLoadedData={() => setShowVideo(true)} // show video when it has loaded
       />
     </div>
   );
